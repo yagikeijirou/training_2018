d0d55f20dc563b19041a485b4e9b8905f8c04689
araki18s <araki@cns.co.jp>
2018-05-25 18:09:40 +0900

    20180525_araki18s_03

--- src/main/resources/static/js/admin/listOutput.js
+++ src/main/resources/static/js/admin/listOutput.js
@@ -1,4 +1,4 @@
-$(function() {
+/*$(function() {
     $('#yearMonthPicker .date').datepicker({
         format: 'yyyymm',
         language: 'ja',
@@ -6,4 +6,15 @@ $(function() {
         minViewMode : 1
     });
 
+});*/
+$(document).ready(function() {
+    var date = new Date();
+
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    if (month < 10) month = "0" + month;
+
+    var today = year + "-" + month;
+    $("#theDate").attr("value", today);
 });
